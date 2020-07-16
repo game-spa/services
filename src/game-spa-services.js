60dc21c2c1c24af26704c0de742b8aca21361c57
Danniel Hugo <danniel@stilingue.com.br>
2020-07-16 14:00:22 -0300

    feat: add findGame service

--- src/game-spa-services.js
+++ src/game-spa-services.js
@@ -1,4 +1,18 @@
-import "./set-public-path";
+import './set-public-path';
+
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://api.rawg.io/api/',
+});
 
 // Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+export async function findGame(name) {
+  const response = await instance.get(`/games?search=${name}`, {
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+
+  return response.data || {};
+}
